40025af5339ab4bd2618610135e2167ac45f708b
honore34 <hmubiayi@terpmail.umd.edu>
2024-11-09 14:48:18 -0500

    added comments
    will help explain what each line is doing currently

--- Ticket_Price_Tracker.py
+++ Ticket_Price_Tracker.py
@@ -5,16 +5,18 @@ import csv
 
 
 def fetch_event_data():
-    url = "https://www.ticketmaster.com/" 
-    response = requests.get(url) 
 
-    parse_html = BeautifulSoup(response.content,"html.parser") 
-    events = parse_html.find_all('div',class_=re.compile('[a-z])'))
+    url = "https://www.ticketmaster.com/" 
+    response = requests.get(url) # sending a request to the website 
+
+    parse_html = BeautifulSoup(response.content,"html.parser") # parse HTML content 
+
+    events = parse_html.find_all('div',class_=re.compile('[a-z])')) # using regex to scape the ticket information 
 
     data = []
 
-
-    with open('Ticket_data.csv','w', newline = '') as csv_conn: 
+    #not used yet but will be the end goal to store all the scaped information. 
+    with open('Ticket_data.csv','w', newline = '') as csv_conn:  
         write = csv.DictWriter(csv_conn,fieldnames= data[0].key())
         write.writeheader()
         write.writerows(data)
